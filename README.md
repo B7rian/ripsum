d460a112f38036c6569d795d1bce54b8944f0d71
Brian Hughes <B7rian@users.noreply.github.com>
2022-10-24 00:41:59 +0000

    20221022 scheduler (#8)
    * Removed taskflow
    
    * Updated README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ripsum - Parallel/Multicore/Threaded sha256sum
+# ripsum - A faster sha256sum
 ripsum is meant to be a fast drop-in replacement for
 ```
 find <dirs...> -type f -exec sha256sum {} +
@@ -9,10 +9,10 @@ find <dirs...> -type f -exec sha256sum {} +
 sha256sum -c <checksum file>
 ```
 
-The name and features are inspired by 
+The name and features are inspired by
 [ripgrep](https://github.com/BurntSushi/ripgrep) which has, overall, a much
 better user experience than the find-grep commandlines it replaces (nothing
-against find or grep of course). I hope ripsum is as pleasent to use as ripgrep
+against find or grep of course). I hope ripsum is as nice to use as ripgrep
 someday. 
 
 # Usage
@@ -21,7 +21,7 @@ To generate checksums: `ripsum <dirs...>`
 The check previously generated checksums in a file:: `ripsum -c <checksum
 file>`
 
-checksum files are interchangable with sha256sum but don't support all features
+Checksum files are interchangable with sha256sum but don't support all features
 yet. In particular, the mode character is ignored and everything is done in
 binary mode. 
 
@@ -39,25 +39,25 @@ cmake --build
 You may need to install the libssl development package for your Linux distro.
 On void, Ubuntu, and Ubuntu derivitives like Zorin it's called libssl-dev. 
 
-ripsum uses [taskflow](https://taskflow.github.io) to run tasks on multiple
-cores and the needed headers from taskflow are included in the ripsum git repo. 
-
 # Testing
 
-`make test` in the cmake build directory will run both sha256sum and ripsum and compare the output. The
-tests are run on the taskflow subdirectory by default but you can change
-run_tests.sh script to use your own dataset. For example, before release I run
-the tests on about 167000 files on a 1TB external drive and make sure the
-results match. 
+`make test` in the cmake build directory will run both sha256sum and ripsum and
+compare the output. The tests are run on the build subdirectory by default
+but you can change run_tests.sh script to use your own dataset.
 
 # Performance
 
+ripsum used to use a lot of threads, but the performance gains weren't 
+there and I'm currently working on benchmarks to determine the best path
+forward.
+
 ## CPU Utilization
-ripsum utilize as much of your CPU as it can and will likely saturate the I/O
-bandwidth on your system. You can infer that your I/O bandwidth is saturated if
-ripsum is running a large job and CPU utilization is less than 100% - in this
-case ripsum can't get the data off of the filesystem fast enough to keep the
-CPU busy.
+
+One of the goals of ripsum is to use as many CPU core/threads as are needed to
+take advantage of the I/O bandwidth on your system. You can infer that your I/O
+bandwidth is saturated if ripsum is running a large job and CPU utilization is
+less than 100% - in this case ripsum can't get the data off of the filesystem
+fast enough to keep the CPU busy. 
 
 Actual speedup vs. find+sha256sum depends on your system. For example, on
 systems with slower cores and/or fast I/O, you might see 4 or more cores
--- src/CScheduler.cpp
+++ src/CScheduler.cpp
@@ -21,7 +21,6 @@
 #include <mutex>
 #include <fstream>
 #include <sstream>
-#include "taskflow/taskflow.hpp"
 
 #include "CScheduler.h"
 #include "CTaskState.h"
@@ -39,218 +38,49 @@ void CScheduler::AddPath(std::filesystem::path aP) {
 
 
 // 
-// Run: Set up taskflow tasks to perform the operations requested by 
-// the user - either check or compute checksums
+// Run: Compute or check checksums depeding on user options.
 //
 void CScheduler::Run(CUserInput& aInput,
 	                 std::function<void(CTaskState*)> aDoneCb)
 {
 	if(aInput.mCheckFlag) {
 		for(auto& target: mvPaths) {
-			mTaskflow.emplace([&, target](tf::Subflow& aSubflow) {
-				aInput.ReadChecksumsFromFile(target,
-					[&](std::filesystem::path aP, std::string aChecksum) { 
-						MakeTasksToHashFile(aSubflow, aP, 
-							[&, aChecksum](CTaskState *apState) {
-								apState->SetExpectedChecksum(aChecksum);
-								aDoneCb(apState);
+			aInput.ReadChecksumsFromFile(target,
+				[&](std::filesystem::path aP, std::string aChecksum) { 
+					HashFile(aP, 
+						[&, aChecksum](CTaskState *apState) {
+							apState->SetExpectedChecksum(aChecksum);
+							aDoneCb(apState);
 						});
 					});
-			});
 		}
 	}
 	else {
 		for(auto& target: mvPaths) {
-			mTaskflow.emplace([&, target](tf::Subflow& aSubflow) {
-				CFileSystem::FindFiles(target,
-					[&](std::filesystem::path aP) { 
-						MakeTasksToHashFile(aSubflow, aP, aDoneCb);
-					});
-			});
+			CFileSystem::FindFiles(target,
+				[&](std::filesystem::path aP) { 
+					HashFile(aP, aDoneCb);
+				});
 		}
 	}
-
-	mExecutor.run(mTaskflow).wait(); 
 }
 
-//
-// MakeTasksToHashFile creates tasks to compute the has for a file.  Note that
-// taskflow requires a dummy task (called loopBack below) to avoid hanging.
-// This is in the taskflow docs somewhere.
-//
-// Pick single-buffer or double-buffered file reads below
-
-#if 0
-
-//
-// This is the single-file-buffer version of the flow.
-//
-// xxx      is a strong dependency
-// --- or | is a week dependency
-//
-//      Init
-//        x
-//        x
-//  +-->Read 
-//  |     x      
-//  |     x    0
-//  |   Check?---+
-//  |     |      |
-//  |     | 1    |
-//  |     v      |
-//  |   Hash     |
-//  |     x      |
-//  |     x      |
-//  +--Loopback  |
-//               |
-//     Finish <--+
-//
-
-void CScheduler::MakeTasksToHashFile(tf::Subflow& aSubflow,
-		std::filesystem::path aP, std::function<void(CTaskState*)> aDoneCb) {
+void CScheduler::HashFile(std::filesystem::path aP, 
+		                  std::function<void(CTaskState*)> aDoneCb) 
+{
 	CTaskState *pState = new CTaskState(aP);
 
-	tf::Task a = aSubflow.emplace([=]() { 
-		pState->Init(); 
-	}).name("init");
+	pState->Init(); 
 
-	tf::Task b = aSubflow.emplace([=]() { 
-		pState->ReadBytes(); 
-	}).name("read");
-
-	tf::Task c = aSubflow.emplace([=]() {
-		if(pState->FileOk() == false) {
-			return 1;
-		}
-		return 0;
-	}).name("check");
-
-	tf::Task d = aSubflow.emplace([=]() { 
+	pState->ReadBytes(); 
+	while(pState->FileOk() == true) {
 		pState->AddBytesToHash(); 
-	}).name("hash");
+		pState->ReadBytes(); 
+	}
+	pState->AddBytesToHash(); 
 
-	tf::Task d2 = aSubflow.emplace([=]() { 
-		return 0; 
-	}).name("loopBack");
-
-	tf::Task e = aSubflow.emplace([=]() {
-		pState->Finish();
-		aDoneCb(pState);
-		delete pState;
-	}).name("finish");
-
-	a.precede(b);
-	b.precede(c);
-	c.precede(d, e);
-	d.precede(d2);
-	d2.precede(b);
+	pState->Finish();
+	aDoneCb(pState);
+	delete pState;
 }
 
-#else
-
-//
-// This is the double-file-buffer version of the flow.  
-//
-// xxx      is a strong dependency
-// --- or | is a week dependency
-//
-//           Init
-//            x
-//    +----->Do
-//    |       x
-//    |   xxxxxxxxx
-//    |   x       x
-//    | Read0    Hash1
-//    |   x       x
-//    |   xxxxxxxxx
-//    |       x
-//    |      Sync
-//    |       x
-//    |   xxxxxxxxx
-//    |   x       x
-//    | Hash0    Read1
-//    |   x       x
-//    |   xxxxxxxxx
-//    |       x
-//    |     While----->Finish
-//    |       |
-//    +-------+
-//           
-// This is more like a do-while loop and doesn't need a special loopback node
-// to avoid deadlock.  The "Do" node is effectively a fork to kick off 2 tasks
-// in parallel (a read and a hash) which the "While" conditional node can't do.
-// The Sync node keeps things coordinated e.g. so the hash nodes don't get to
-// far ahead of the reads and vice versa. 
-//
-
-
-void CScheduler::MakeTasksToHashFile(tf::Subflow& aSubflow,
-		std::filesystem::path aP, std::function<void(CTaskState*)> aDoneCb) {
-
-	CTaskState *pState = new CTaskState(aP);
-
-	tf::Task init = aSubflow.emplace([=]() { 
-		pState->Init(); 
-		pState->ReadBytes(); // Prime the loop
-	}).name("init");
-
-	init.acquire(mMemLimiter);
-
-	tf::Task do_task = aSubflow.emplace([=]() {
-		//std::cerr << "--do--" << std::endl;
-	}).name("do_task");
-
-	init.precede(do_task);
-
-	tf::Task read0 = aSubflow.emplace([=]() { 
-		pState->ReadBytes(); 
-	}).name("read0");
-
-	tf::Task hash1 = aSubflow.emplace([=]() { 
-		pState->AddBytesToHash(); 
-	}).name("hash1");
-
-	read0.succeed(do_task);
-	hash1.succeed(do_task);
-
-	tf::Task sync = aSubflow.emplace([=]() {
-		//std::cerr << "--sync--" << std::endl;
-	}).name("sync");
-
-	read0.precede(sync);
-	hash1.precede(sync);
-
-	tf::Task hash0 = aSubflow.emplace([=]() { 
-		pState->AddBytesToHash(); 
-	}).name("hash0");
-
-	tf::Task read1 = aSubflow.emplace([=]() { 
-		pState->ReadBytes(); 
-	}).name("read1");
-
-	hash0.succeed(sync);
-	read1.succeed(sync);
-
-	tf::Task while_task = aSubflow.emplace([=]() {
-		//std::cerr << "--while--" << std::endl;
-		if(pState->FileOk() == false) {
-			return 1;
-		}
-		return 0;
-	}).name("while_task");
-
-	hash0.precede(while_task);
-	read1.precede(while_task);
-
-	tf::Task finish = aSubflow.emplace([=]() {
-		//std::cerr << "--finish--" << std::endl;
-		pState->Finish();
-		aDoneCb(pState);
-		delete pState;
-	}).name("finish");
-
-	while_task.precede(do_task, finish);
-	finish.release(mMemLimiter);
-}
-
-#endif
